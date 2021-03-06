d77b8ccb6b6e58e9f56b822405891f889e9a3c9c
Frans <infofvae@gmail.com>
2021-03-06 12:04:58 +0100

    CoinRequest link updated

--- js/dash/index.js
+++ js/dash/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {	
 	//Load the banner
-	$("#sidebar").append('<br><br><small>Donate using CoinRequest:</small><br><a href="https://coinrequest.io/request/pThndSEc2vy1X1b" target="_blank"><img class="img-responsive" src="https://g-dash.nl/images/coinrequest.png" border="0"></a>');	
+	$("#sidebar").append('<br><br><small>Donate using CoinRequest:</small><br><a href="https://coinrequest.io/request/DBM3pDrfci0EIpA" target="_blank"><img class="img-responsive" src="images/coinrequest.png" border="0"></a>');	
 });
